b96e90d613c93c82a0a6373b3974e6b708f83a71
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-01-01 09:42:52 +1100

    Experimenting with changing menu title

--- AESCrypto/src/com/evgenii/aescrypto/MainActivity.java
+++ AESCrypto/src/com/evgenii/aescrypto/MainActivity.java
@@ -2,13 +2,19 @@ package com.evgenii.aescrypto;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
+
+	public String currentDecryptMenuTitle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		currentDecryptMenuTitle = getResources().getString(
+				R.string.menu_decrypt_title);
 		setContentView(R.layout.activity_main);
 	}
 
@@ -17,5 +23,17 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		menu.findItem(R.id.action_decrypt).setTitle(currentDecryptMenuTitle);
+		Log.d("ii", currentDecryptMenuTitle);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	public void onSendClicked(View view) {
+		currentDecryptMenuTitle = "Hey :)";
+		invalidateOptionsMenu();
 	}
 }
