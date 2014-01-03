478a599b403044a84d0b78fb1bc460e6c85e55b0
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-01-03 13:14:17 +1100

    Continue developing JsRunner

--- AESCrypto/src/com/evgenii/aescrypto/JSRunner.java
+++ AESCrypto/src/com/evgenii/aescrypto/JSRunner.java
@@ -1,16 +1,46 @@
 package com.evgenii.aescrypto;
 
+import java.util.ArrayList;
+
 import android.content.Context;
+import android.webkit.WebView;
 
 /**
  * Executes JavaScript code. Loads JS code. Calls a JavaScript function. Allows
  * to pass an argument to function call. Stores the function's return value.
  */
-public class JSRunner {
-	protected Context mContext;
+public class JsRunner {
+	private final Context mContext;
+	private WebView mWebView;
+	private ArrayList<String> mInitialJS;
 
-	public JSRunner(Context context) {
+	public JsRunner(Context context) {
 		mContext = context;
+	}
+
+	public WebView getWebView() {
+		initWebView();
+		return mWebView;
+	}
+
+	public ArrayList<String> getInitialJs() {
+		if (mInitialJS == null) {
+			mInitialJS = new ArrayList<String>();
+		}
+		return mInitialJS;
+	}
+
+	public void addInitialJs(String js) {
+		ArrayList<String> initialJs = getInitialJs();
+		initialJs.add(js);
+	}
+
+	private void initWebView() {
+		if (mWebView != null)
+			return;
+
+		mWebView = new WebView(mContext);
+		mWebView.loadData("", "text/html", null);
 	}
 
 }
--- AESCryptoTest/src/com/evgenii/aescrypto/test/JSRunnerTest.java
+++ AESCryptoTest/src/com/evgenii/aescrypto/test/JSRunnerTest.java
@@ -1,18 +1,31 @@
 package com.evgenii.aescrypto.test;
 
-import android.test.InstrumentationTestCase;
+import android.test.AndroidTestCase;
+import android.webkit.WebView;
 
-import com.evgenii.aescrypto.JSRunner;
+import com.evgenii.aescrypto.JsRunner;
 
-public class JSRunnerTest extends InstrumentationTestCase {
-	protected JSRunner mJSRunner;
+public class JsRunnerTest extends AndroidTestCase {
+	protected JsRunner mJsRunner;
 
 	@Override
 	protected void setUp() {
-		mJSRunner = new JSRunner(getInstrumentation().getContext());
+		mJsRunner = new JsRunner(mContext);
 	}
 
 	public void testRun() {
 		assertTrue(true);
+	}
+
+	public void testGetWebView_shouldCreateWebView() {
+		WebView webView = mJsRunner.getWebView();
+		assertNotNull(webView);
+	}
+
+	public void testAddInitialJs() {
+		mJsRunner.addInitialJs("function first(){};");
+		mJsRunner.addInitialJs("function second(){};");
+		assertEquals(2, mJsRunner.getInitialJs().size());
+		assertEquals("function first(){};", mJsRunner.getInitialJs().get(0));
 	}
 }
