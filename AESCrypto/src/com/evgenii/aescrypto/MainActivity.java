68053ed88c2238be8668d83f7ff0defef8744764
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-01-12 12:35:50 +1100

    Continue development of JsRunner

--- AESCrypto/src/com/evgenii/aescrypto/MainActivity.java
+++ AESCrypto/src/com/evgenii/aescrypto/MainActivity.java
@@ -26,26 +26,12 @@ public class MainActivity extends Activity {
 				R.string.menu_decrypt_title);
 		setContentView(R.layout.activity_main);
 
-		// myWebView = new WebView(this);
-		//
-		// myWebView.loadData("", "text/html", null);
-		// myWebView.loadUrl("javascript: function wow(){return 'cuul!';}");
-		// myWebView.loadUrl("javascript: function oh(){return 'holy!';}");
-		//
-		// final WebSettings webSettings = myWebView.getSettings();
-		// webSettings.setJavaScriptEnabled(true);
-		// myJSInterface = new JavaScriptInterface();
-		// myWebView.addJavascriptInterface(myJSInterface, "Android");
-
 		jsRunner = new JsRunner(this);
 
 		try {
 			jsRunner.addInitialJs("function greet(name) { return 'Hello, ' + name; } ");
-			Log.d("ii", "addInitialJs()");
 			jsRunner.runInitialJs();
-			Log.d("ii", "runInitialJs()");
 		} catch (final InitialJsHasAlreadyBeenRun e) {
-			Log.e("ii", "Error in initial JS");
 			e.printStackTrace();
 		}
 	}
@@ -60,24 +46,14 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
 		menu.findItem(R.id.action_decrypt).setTitle(currentDecryptMenuTitle);
-		Log.d("ii", currentDecryptMenuTitle);
 		return super.onPrepareOptionsMenu(menu);
 	}
 
 	public void onSendClicked(View view) {
-		// myWebView.loadUrl("javascript: Android.sendResult(wow());");
-		// myWebView.loadUrl("javascript: Android.sendResult(oh());");
-		// myWebView
-		// .loadUrl("javascript: Android.sendResult(Android.getString());");
-		//
-		// myWebView
-		// .loadUrl("javascript: Android.sendResult(Android.getString());");
-
 		jsRunner.runJsFunction("greet", "Evgenii", new JsCallback() {
 			@Override
 			public void onResult(String value) {
-				Log.d("ii", "result of greet()");
-				Log.d("ii", value);
+				Log.d("ii", String.format("result of greet(): %s", value));
 			}
 		});
 		currentDecryptMenuTitle = "Hey :)";
