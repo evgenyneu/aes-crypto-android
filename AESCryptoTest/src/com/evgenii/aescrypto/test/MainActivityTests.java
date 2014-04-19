4baf334a3bf3617700fd5a8eb49858a6a36803bc
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-04-19 11:51:21 +1000

    Integration test for uncryption/decryption

--- AESCryptoTest/src/com/evgenii/aescrypto/test/MainActivityTests.java
+++ AESCryptoTest/src/com/evgenii/aescrypto/test/MainActivityTests.java
@@ -21,6 +21,36 @@ public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActi
 		mActivity = getActivity();
 	}
 
+	public void testDecrypt() throws InterruptedException {
+		final JsEncryptor encryptor = mActivity.getEncryptor();
+		final ArrayList<String> myResult = new ArrayList<String>();
+
+		getInstrumentation().runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				encryptor
+						.decrypt(
+								"AESCryptoV108f46e2fb15f50e9c170442ec5ec70e6fcded6378b13f1a659f0eb65e8eddb2335de8e76be90b2f0a",
+								"test", new JsCallback() {
+									@Override
+									public void onResult(String result) {
+										myResult.add(result);
+									}
+								});
+			}
+		});
+
+		for (int i = 0; i < 100; i++) {
+			Thread.sleep(50);
+			if (myResult.size() == 1) {
+				break;
+			}
+		}
+
+		assertEquals(1, myResult.size());
+		assertEquals("My Test Message 日本", myResult.get(0));
+	}
+
 	public void testEncrypt() throws InterruptedException {
 		final JsEncryptor encryptor = mActivity.getEncryptor();
 		final ArrayList<String> myResult = new ArrayList<String>();
@@ -46,5 +76,37 @@ public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActi
 
 		assertEquals(1, myResult.size());
 		assertEquals("AESCryptoV10", myResult.get(0).substring(0, 12));
+	}
+
+	public void testEncryptAndDecrypt() throws InterruptedException {
+		final JsEncryptor encryptor = mActivity.getEncryptor();
+		final ArrayList<String> myResult = new ArrayList<String>();
+
+		getInstrumentation().runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				encryptor.encrypt("Hello there", "my password", new JsCallback() {
+					@Override
+					public void onResult(String resultEncrypt) {
+						encryptor.decrypt(resultEncrypt, "my password", new JsCallback() {
+							@Override
+							public void onResult(String resultDecrypt) {
+								myResult.add(resultDecrypt);
+							}
+						});
+					}
+				});
+			}
+		});
+
+		for (int i = 0; i < 100; i++) {
+			Thread.sleep(50);
+			if (myResult.size() == 1) {
+				break;
+			}
+		}
+
+		assertEquals(1, myResult.size());
+		assertEquals("Hello there", myResult.get(0));
 	}
 }
